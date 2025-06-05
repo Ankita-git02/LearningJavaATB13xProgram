8408c6b7f0af4f168e8b237e7d713ac6bfccfe50
ankitabhujbal <bhujbalankita02@gmail.com>
2025-06-05 22:25:57 +0530

    1st commit

---
+++ src/ex_01_Java_Basic/Lab001_HelloWord.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab001_HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+        System.out.println(2+3);
+    }
+}
