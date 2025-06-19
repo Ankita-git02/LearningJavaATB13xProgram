ef72449bf393b2939026a21d4a47930e7d74a106
ankitabhujbal <bhujbalankita02@gmail.com>
2025-06-19 08:14:25 +0530

    Java challage 19-2

---
+++ src/javaTestExamples/Challage2_arithmatic_assignment_oprtaor.java
@@ -0,0 +1,29 @@
+package javaTestExamples;
+
+import java.util.Scanner;
+
+public class Challage2_arithmatic_assignment_oprtaor {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter value 1");
+        int a= sc.nextInt();
+        System.out.println("Enter value 2");
+        int b= sc.nextInt();
+
+        System.out.println("Addition = "+(a+b));
+        System.out.println("substraction= "+(a-b));
+        System.out.println("multiplication = "+(a*b));
+        System.out.println("Division= "+(a/b));
+        System.out.println("Modulus = "+(a%b));
+
+        System.out.println("Comapre = "+(a==b));
+
+
+
+
+
+
+
+
+    }
+}
